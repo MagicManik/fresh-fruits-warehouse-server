45588a03d65a0968bc0b68421fe745f5cabe6617
MagicManik <manikislam1952@gmail.com>
2022-05-06 20:45:48 +0600

    succesfully collection server to database

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
 // _______ MIDDLEWARE _______
 app.use(cors());
 app.use(express.json());
+
+
+
+// _______ CONNECTION SERVER TO DATABASE _______
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wceiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('Check Database Collection')
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
+
 
 
 app.get('/', (req, res) => {
