298d575f32f560c03d977c844a7cc2a8d05b2728
MagicManik <manikislam1952@gmail.com>
2022-05-06 20:57:43 +0600

    multiple data loaded from database

--- index.js
+++ index.js
@@ -15,12 +15,34 @@ app.use(express.json());
 // _______ CONNECTION SERVER TO DATABASE _______
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wceiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('Check Database Collection')
-    // perform actions on the collection object
-    client.close();
-});
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoryCollection = client.db('freshFruitsWarehouse').collection('inventory');
+
+
+
+        // _______ TO LOAD DATA FROM DATABASE _____
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+
+
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(console.div);
 
 
 
