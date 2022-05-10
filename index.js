61f78b791f866731adc552fb8bafc94a8c937ec7
MagicManik <manikislam1952@gmail.com>
2022-05-11 01:10:14 +0600

    update server

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ function verifyJWT(req, res, next) {
 
 
 
-
 // _______ TO CONNECT SERVER TO DATABASE _______
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wceiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
